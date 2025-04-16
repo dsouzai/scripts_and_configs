52d06e2eb0be7170551e4eda441ca2aeb622d4ef
Irwin D'Souza <dsouzai.gh@gmail.com>
2025-04-16 16:53:35 -0400

    Update check_reboot.sh

--- check_reboot.sh
+++ check_reboot.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-echo "Resetting ulimit -c 0"
-ulimit -c 0
-
-echo "Resetting coredump pattern to apport"
-sysctl -w kernel.core_pattern="|/usr/share/apport/apport -p%p -s%s -c%c -d%d -P%P -u%u -g%g -- %E"
-
-dsouzai@ub-cascadelake-2s32c-03:~/OL-24.0.0.6/mpirvu/AcmeAir$ cat ~/check_reboot.sh
-#!/bin/bash
-
 user_date=$(last --time-format=iso $USER | head -2 | tail -1 | perl -pe 's/.*(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d).*/$1/g')
 reboot_date=$(last --time-format=iso reboot | head -1 | perl -pe 's/.*(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d).*/$1/g')
 
